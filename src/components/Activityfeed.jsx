983a9664320e6c79c20cc945307112b3aa24f821
nawfallodhi <nawfal.lodhi@gmail.com>
2026-05-13 20:09:25 +0500

    Minor bug, file name changed

--- src/components/Activityfeed.jsx
+++ src/components/Activityfeed.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useCallback } from "react";
 
 export default function ActivityFeed({ events }) {
   const topRef = useRef(null);
